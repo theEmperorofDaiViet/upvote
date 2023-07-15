6a9272855eccfcf43198bb9d41c347e1d8edfbbc
theEmperorofDaiViet <Khiet.To.Development@gmail.com>
2023-07-16 00:50:20 +0700

    first commit

---
+++ src/seed.js
@@ -0,0 +1,41 @@
+window.Seed = (function() {
+    const submissions = [
+        {
+            id: 1,
+            title: "Deciding To Be Better",
+            description: "If you're struggling with feeling like a failure, do this and you won't anymore.",
+            url: "#",
+            votes: 16,
+            avatar: "../public/images/avatars/daniel.jpg",
+            submissionImage: "../public/images/submissions/image-yellow.png"
+        },
+        {
+            id: 2,
+            title: "Get Disciplined!",
+            description: "If you want to master the Pomodoro technique, you need to use breaks wisely.",
+            url: "#",
+            votes: 13,
+            avatar: "../public/images/avatars/kristy.png",
+            submissionImage: "../public/images/submissions/image-rose.png"
+        },
+        {
+            id: 3,
+            title: "Get Motivated!",
+            description: "Ideas not coupled with action never become bigger than the brain cells they occupied.",
+            url: "#",
+            votes: 19,
+            avatar: "../public/images/avatars/veronika.jpg",
+            submissionImage: "../public/images/submissions/image-steel.png"
+        },
+        {
+            id: 4,
+            title: "Continuously Improve Yourself",
+            description: "I disappeared for one year to improve myself and here's what happened.",
+            url: "#",
+            votes: 15,
+            avatar: "../public/images/avatars/molly.png",
+            submissionImage: "../public/images/submissions/image-aqua.png"
+        }
+    ];
+    return { submissions: submissions };
+}());
